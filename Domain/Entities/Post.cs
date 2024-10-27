3d229c32c410e68a5d0e93f1267d63d666fc3efc
HilariMF <133549336+HilariMF@users.noreply.github.com>
2024-10-26 21:19:26 -0400

    first commit

---
+++ Domain/Entities/Post.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities
+{
+    public class Post : BaseEntity
+    {
+        public int Id { get; set; }
+        public string Content { get; set; }
+        public string UserId { get; set; }  // Almacena el ID del usuario
+        public int TopicId { get; set; }     // Relación con el tema
+        public Topic Topic { get; set; }
+    }
+}
